ff612cd746c0d4b89fd9ee066fb3e5e02721c755
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-11-11 16:23:27 +0300

    practice doc

---
+++ test/test.rb
@@ -0,0 +1,25 @@
+# numbah 1
+# def double (number)
+#     doubled_number = number * 3
+#     return doubled_number
+# end
+
+# puts double(3)
+# puts double(4)
+
+
+# numbah 2
+# def hello (name, greeting, small_talk, punctuation)
+#     return "#{greeting} #{name}, #{small_talk} #{punctuation}"
+# end
+
+# puts hello('hisham', 'how goes its', 'nice weather were having', '?!')
+
+
+# numbah 3
+def drink_milk(thirsty = true)
+    return "I'm not thirsty" unless true
+    'mmmm......milk'
+end
+
+puts drink_milk
